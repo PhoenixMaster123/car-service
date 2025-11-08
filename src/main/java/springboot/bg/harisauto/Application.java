d9d9566460ad30c99819cf34df75b129df09e9c9
Kristian Popov <kristianpopov03@yahoo.com>
2025-11-08 22:22:34 +0100

    init

---
+++ src/main/java/springboot/bg/harisauto/Application.java
@@ -0,0 +1,17 @@
+package springboot.bg.harisauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main application class for the Car Service application.
+ *
+ * @author Kristian Popov
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
