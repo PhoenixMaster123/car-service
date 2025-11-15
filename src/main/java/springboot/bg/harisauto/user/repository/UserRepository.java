ebb5d60ce540ec07c81c9e065861a5f2a6c01e5e
Popov, Kristian <kristian.popov@study.thws.de>
2025-11-16 00:25:34 +0100

    HCC-4: feat: add and correct the checkstyle of the classes

--- src/main/java/springboot/bg/harisauto/user/repository/UserRepository.java
+++ src/main/java/springboot/bg/harisauto/user/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package springboot.bg.harisauto.user.repository;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import springboot.bg.harisauto.user.model.User;
-import java.util.Optional;
-import java.util.UUID;
 
 /**
  * UserRepository.java - Repository interface for User entity.
