4c42550d2a9cc3c6dc2d49434c256ecd930e98e6
Popov, Kristian <kristian.popov@study.thws.de>
2025-11-16 00:39:12 +0100

    HCC-4: fix: The test was fixed

--- src/test/java/springboot/bg/harisauto/ApplicationTests.java
+++ src/test/java/springboot/bg/harisauto/ApplicationTests.java
@@ -2,9 +2,20 @@ package springboot.bg.harisauto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import springboot.bg.harisauto.chatbot.service.GeminiService;
+import springboot.bg.harisauto.email.EmailService;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
+
+    @MockitoBean
+    private EmailService emailService;
+
+    @MockitoBean
+    private GeminiService geminiService;
 
     @Test
     void contextLoads() {
